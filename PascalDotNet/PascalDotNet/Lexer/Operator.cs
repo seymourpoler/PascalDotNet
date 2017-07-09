40cdb944632b6ce7043d6d336d1911db7968e20b
Seymour Poler <seymourpoler@gmail.com>
2017-07-09 07:01:49 +0200

    Adding an enum of operations

---
+++ PascalDotNet/PascalDotNet/Lexer/Operator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PascalDotNet.Lexer
+{
+	public enum Operator
+	{
+		Add,
+		Sub,
+		Mul,
+		Div,
+		Percent,
+		Assign,
+		And,
+		Or,
+		Equals,
+		NotEquals,
+		Greater,
+		Less,
+		GreaterEqual,
+		LessEqual
+	}
+}
