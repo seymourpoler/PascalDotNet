8f3401dee360bc34ab1eb3d82c05e7b9879fae91
Seymour Poler <seymourpoler@gmail.com>
2017-07-12 07:46:26 +0200

    adding node clase

---
+++ PascalDotNet/PascalDotNet/Lexer/Node.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.ObjectModel;
+using System.Collections.Generic;
+
+namespace PascalDotNet.Lexer
+{
+	public class Node
+	{
+		private List<Node> _nodes;
+
+		public ReadOnlyCollection<Node> Nodes{get{return _nodes.AsReadOnly ();}}
+		public string Name { get; private set;}
+
+		public Node (string name)
+		{
+			_nodes = new List<Node> ();
+			Name = name;
+		}
+	}
+}
