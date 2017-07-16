955312548974626df189f5f556333bff0582b47d
Seymour Poler <seymourpoler@gmail.com>
2017-07-16 06:32:46 +0200

    green ReturnsFalseWhenOneIsNullAndNotOther

---
+++ PascalDotNet/PascalDotNet/Lexer.Tests/Tokens/TokenComparatorTest.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using PascalDotNet.Lexer.Tokens;
+using FluentAssertions;
+
+namespace PascalDotNet.Lexer.Tests.Tokens
+{
+	[TestFixture]
+	public class TokenComparatorTest
+	{
+		[Test]
+		public void ReturnsTrueWhenBothNull()
+		{
+			var result = TokenComparator.Equals (null, null);
+
+			result.Should ().BeTrue ();
+		}
+
+		[Test]
+		public void ReturnsFalseWhenOneIsNullAndNotOther()
+		{
+			var result = TokenComparator.Equals (new KeyWordToken(""), null);
+
+			result.Should ().BeFalse ();
+		}
+	}
+}
