180d965aecf9d814de0e1c2dd43ae972d7cac22d
Seymour Poler <seymourpoler@gmail.com>
2017-07-22 07:00:53 +0200

    ignore format

--- PascalDotNet/PascalDotNet/Lexer/Consts.cs
+++ PascalDotNet/PascalDotNet/Lexer/Consts.cs
@@ -2,7 +2,7 @@
 {
 	public class Consts
 	{
-		public const string CONSTANT_DECLARATION = "Const";
 		public const string PROGRAM_HEADING = "Heading";
+		public const string CONSTANT_DECLARATION = "Constant";
 	}
 }
