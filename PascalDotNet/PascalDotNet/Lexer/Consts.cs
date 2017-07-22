e28de5639ea385dcd7c7cdc53bdd376c0bac3455
Seymour Poler <seymourpoler@gmail.com>
2017-07-22 07:01:55 +0200

    adding plural

--- PascalDotNet/PascalDotNet/Lexer/Consts.cs
+++ PascalDotNet/PascalDotNet/Lexer/Consts.cs
@@ -3,7 +3,7 @@
 	public class Consts
 	{
 		public const string PROGRAM_HEADING = "Heading";
-		public const string CONSTANT_DECLARATION = "Constant";
+		public const string CONSTANTS_DECLARATION = "Constants";
 		public const string VARIABLES_DECLARATION = "Variables";
 	}
 }
