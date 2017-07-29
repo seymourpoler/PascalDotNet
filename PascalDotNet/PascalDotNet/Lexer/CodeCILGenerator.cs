2e77a1551644df2ce24043a3484070a9ca214990
Seymour Poler <seymourpoler@gmail.com>
2017-07-29 06:16:43 +0200

    define CIL code generator class

--- PascalDotNet/PascalDotNet/Lexer/CodeCILGenerator.cs
+++ PascalDotNet/PascalDotNet/Lexer/CodeCILGenerator.cs
@@ -1,10 +1,28 @@
-﻿namespace PascalDotNet.Lexer
+﻿using System.Threading;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace PascalDotNet.Lexer
 {
 	public class CodeCILGenerator
 	{
+		AssemblyBuilder assemblyBuilder;
+
 		public CodeCILGenerator ()
 		{
+			var domain = Thread.GetDomain();
+			var assembyName = new AssemblyName(){
+				Version = new System.Version(0,0,0,1),
+				//TODO: change
+				Name = "OutPut"};
+			assemblyBuilder = domain.DefineDynamicAssembly (assembyName, AssemblyBuilderAccess.Save);
 		}
+
+		public void Generate()
+		{
+			
+		}
+
 	}
 }
 
