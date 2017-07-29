0cd23448c5f147f96b6f6911d2520a7b6a7f395c
Seymour Poler <seymourpoler@gmail.com>
2017-07-29 06:31:53 +0200

    defining console applicationi with entry poiny

--- PascalDotNet/PascalDotNet/Lexer/CodeCILGenerator.cs
+++ PascalDotNet/PascalDotNet/Lexer/CodeCILGenerator.cs
@@ -1,12 +1,14 @@
-﻿using System.Threading;
+﻿using System;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Threading;
 
 namespace PascalDotNet.Lexer
 {
 	public class CodeCILGenerator
 	{
 		AssemblyBuilder assemblyBuilder;
+		ModuleBuilder moduleBuilder;
 
 		public CodeCILGenerator ()
 		{
@@ -16,13 +18,23 @@ namespace PascalDotNet.Lexer
 				//TODO: change
 				Name = "OutPut"};
 			assemblyBuilder = domain.DefineDynamicAssembly (assembyName, AssemblyBuilderAccess.Save);
+			moduleBuilder = assemblyBuilder.DefineDynamicModule(assembyName.Name, "OutPut.exe"/*TODO: change*/);
+			assemblyBuilder.SetEntryPoint (BuildMainMethod(), PEFileKinds.ConsoleApplication);
 		}
 
 		public void Generate()
 		{
-			
+			throw new NotImplementedException ();
 		}
 
+		private MethodBuilder BuildMainMethod()
+		{
+			var type = moduleBuilder.DefineType("Application",
+				TypeAttributes.Public|TypeAttributes.Class);
+			return type.DefineMethod("Main",
+				MethodAttributes.Public|
+				MethodAttributes.Static,
+				typeof(int), new Type[] { typeof(string[]) });
+		}
 	}
 }
-
