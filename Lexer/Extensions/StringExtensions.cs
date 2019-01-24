bdaa77362134784bb706d92bbefc8bc7342bc5b0
Seymour Poler <seymourpoler@gmail.com>
2019-01-24 16:20:17 +0100

    use class instead type

--- Lexer/Extensions/StringExtensions.cs
+++ Lexer/Extensions/StringExtensions.cs
@@ -1,10 +1,12 @@
-﻿namespace PascalDotNet.Lexer.Extensions
+﻿using System;
+
+namespace PascalDotNet.Lexer.Extensions
 {
 	public static class StringExtensions
 	{
 		public static bool IsEqualsTo(this string strA, string strB)
 		{
-			return string.Compare (strA: strA, strB: strB, ignoreCase: true) == 0;
+			return String.Compare (strA, strB, StringComparison.OrdinalIgnoreCase) == 0;
 		}
 	}
 }
