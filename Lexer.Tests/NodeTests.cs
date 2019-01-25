702552a597e84b96fca545aec7cf9cc8bba89608
seymourpoler <seymourpoler@gmail.com>
2019-01-26 00:04:49 +0100

    green ReturnNewNodeWithAddedNodes

---
+++ Lexer.Tests/NodeTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PascalDotNet.Lexer.Tests
+{
+    [TestFixture]
+    public class NodeTests
+    {
+        [Test]
+        public void ReturnNewNodeWithAddedNodes()
+        {
+            var node = new Node(String.Empty);
+
+            var result = node.Add(new Node(String.Empty));
+
+            result.Nodes.Should().BeOfType<ReadOnlyCollection<Node>>();
+            result.Nodes.Should().NotBeEmpty();
+        }
+    }
+}
