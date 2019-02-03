971dbb98dcf21101c814b0524dd262ffc7afa32d
seymourpoler <seymourpoler@gmail.com>
2019-02-03 16:41:18 +0100

    adding TypesDeclarationParser file and its test file

---
+++ Lexer.Tests/Parsers/TypesDeclarationParserTests.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using NUnit.Framework;
+using PascalDotNet.Lexer.Parsers;
+
+namespace PascalDotNet.Lexer.Tests.Parsers
+{
+    [TestFixture]
+    public class TypesDeclarationParserTests
+    {
+        Mock<ITokensParser> tokensParser;
+        TypesDeclarationParser parser;
+
+        [SetUp]
+        public void SetUp()
+        {
+            tokensParser = new Mock<ITokensParser>();
+            parser = new TypesDeclarationParser(tokensParser.Object);
+        }
+    }
+}
---
+++ Lexer/Parsers/TypesDeclarationParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PascalDotNet.Lexer.Parsers
+{
+    public class TypesDeclarationParser
+    {
+        private readonly ITokensParser _tokensParser;
+
+        public TypesDeclarationParser(ITokensParser tokensParser)
+        {
+            _tokensParser = tokensParser;
+        }
+
+        public Node Parse()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
