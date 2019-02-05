bc7cd98744e01085aebf570fa8647753a50d84bf
Seymour Poler <seymourpoler@gmail.com>
2019-02-05 18:55:30 +0100

    remove not needed test

--- Lexer.Tests/ParserTests.cs
+++ Lexer.Tests/ParserTests.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Linq;
-using FluentAssertions;
-using Moq;
+﻿using Moq;
 using NUnit.Framework;
-using PascalDotNet.Lexer.Exceptions;
-using PascalDotNet.Lexer.Extensions;
-using PascalDotNet.Lexer.Tokens;
 
 namespace PascalDotNet.Lexer.Tests
 {
@@ -21,33 +15,6 @@ namespace PascalDotNet.Lexer.Tests
 			tokensParser = new Mock<ITokensParser> ();
 			parser = new Parser (
 				tokensParser: tokensParser.Object);
-		}
-
-		[Test]
-		public void ParseVariablesDeclaration()
-		{
-			tokensParser
-				.SetupSequence (x => x.WhereTheNextToken (It.IsAny<Func<IToken, bool>>()))
-				.Returns (false)
-				.Returns (true)
-				.Returns (true);
-
-			tokensParser.SetupSequence (x => x.NextToken)
-				.Returns(new ProgramToken ())
-				.Returns(new IdentifierToken ("Test"))
-				.Returns(new SemiColonToken ())
-				.Returns(new VarToken())
-				.Returns(new IdentifierToken("position"))
-				.Returns(new ColonToken())
-				.Returns(new KeyWordToken("INTEGER"))
-				.Returns(new SemiColonToken ())
-				.Returns(new EndOfFileToken());
-
-			var result = parser.Parse ();
-
-			result.Nodes.Third ().Name.Should ().Be (Consts.VARIABLES_DECLARATION);
-			result.Nodes.Third ().Nodes.First().Name.Should ().Be ("position");
-			result.Nodes.Third ().Nodes.First().Nodes.First().Name.Should ().Be ("INTEGER");
 		}
 	}
 }
