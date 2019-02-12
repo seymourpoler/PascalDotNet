a2f6097049f552439a6149822b585dd0068aaa35
Seymour Poler <seymourpoler@gmail.com>
2019-02-12 18:43:15 +0100

    green ThrowsExceptionWhenTheConditionIsFalse

---
+++ Lexer.Tests/CheckTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace PascalDotNet.Lexer.Tests
+{
+    [TestFixture]
+    public class CheckTests
+    {
+        [Test]
+        public void ThrowsExceptionWhenTheConditionIsFalse()
+        {
+            Action action = () => Check.ThrowIf<Exception>(() => true);
+
+            action.Should().Throw<Exception>();
+        }
+    }
+}
---
+++ Lexer/Check.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PascalDotNet.Lexer
+{
+    public static class Check
+    {
+        public static void ThrowIf<TException>(Func<bool> condition)
+        {
+            if (condition.Invoke())
+            {
+                var exception = (Exception)Activator.CreateInstance(typeof(TException));
+                throw exception;
+            }
+        }
+    }
+}
